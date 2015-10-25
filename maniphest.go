8075496835f3d0c5f3e5bb7fd3d3e05dc06d81f3
Eduardo Trujillo <ed@chromabits.com>
2015-10-25 02:52:02 -0400

    Add missing method call

---
+++ maniphest.go
@@ -0,0 +1,19 @@
+package gonduit
+
+import (
+	"github.com/etcinit/gonduit/requests"
+	"github.com/etcinit/gonduit/responses"
+)
+
+// ManiphestQuery performs a call to maniphest.query.
+func (c *Conn) ManiphestQuery(req requests.ManiphestQueryRequest) (*responses.ManiphestQueryResponse, error) {
+	var res responses.ManiphestQueryResponse
+
+	req.Session = c.Session
+
+	if err := c.Call("maniphest.query", &req, &res); err != nil {
+		return nil, err
+	}
+
+	return &res, nil
+}
