5b8d9f852c0edc41dbc0863ae6b0cd1f900e1388
Eduardo Trujillo <ed@chromabits.com>
2015-10-25 05:51:02 -0400

    Play with adding helpers to responses

--- responses/maniphest_query.go
+++ responses/maniphest_query.go
@@ -4,3 +4,12 @@ import "github.com/etcinit/gonduit/entities"
 
 // ManiphestQueryResponse is the response of calling maniphest.query.
 type ManiphestQueryResponse map[string]*entities.ManiphestTask
+
+// Get gets the task with the speicfied numeric ID.
+func (res ManiphestQueryResponse) Get(key string) *entities.ManiphestTask {
+	if _, ok := res[key]; ok {
+		return res[key]
+	}
+
+	return nil
+}
