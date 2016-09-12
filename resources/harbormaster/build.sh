48a9333194e87358e49765e4bb6419dbdc0ff03d
Eduardo Trujillo <ed@chromabits.com>
2016-09-12 13:07:22 -0700

    fix(harbormaster): Clean up directory before running tests

--- resources/harbormaster/build.sh
+++ resources/harbormaster/build.sh
@@ -2,6 +2,8 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+rm -r /home/drydock/go/src
+
 mkdir -p /home/drydock/go/src/github.com/etcinit
 ln -sf $(pwd) /home/drydock/go/src/github.com/etcinit/
 cd /home/drydock/go/src/github.com/etcinit/gonduit
