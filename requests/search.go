dc85ff42991cd1bd9aa0391326e4c52611efbd0b
Peter Ebden <peter.ebden@gmail.com>
2019-02-07 20:23:28 +0000

    try omitempty

--- requests/search.go
+++ requests/search.go
@@ -4,11 +4,11 @@ import "github.com/thought-machine/gonduit/constants"
 
 // SearchRequest represents a request to one of the .search endpoints.
 type SearchRequest struct {
-	QueryKey    string                 `json:"queryKey"`
-	Constraints map[string]interface{} `json:"constraints"`
-	Attachments map[string]bool        `json:"attachments"`
-	Order       constants.SearchOrder  `json:"order"`
-	Before      string                 `json:"before"`
-	After       string                 `json:"after"`
-	Limit       int                    `json:"limit"`
+	QueryKey    string                 `json:"queryKey,omitempty"`
+	Constraints map[string]interface{} `json:"constraints,omitempty"`
+	Attachments map[string]bool        `json:"attachments,omitempty"`
+	Order       constants.SearchOrder  `json:"order,omitempty"`
+	Before      string                 `json:"before,omitempty"`
+	After       string                 `json:"after,omitempty"`
+	Limit       int                    `json:"limit,omitempty"`
 }
