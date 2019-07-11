cfa7049f7714111adf9978923a0c0f7e46270dbf
hstanley <hstanley@thoughtmachine.net>
2019-07-11 17:50:52 +0100

    Default to a 30s timeout

--- core/http.go
+++ core/http.go
@@ -21,12 +21,18 @@ type ClientOptions struct {
 
 // makeHttpClient creates a new HTTP client for making API requests.
 func makeHTTPClient(options *ClientOptions) *http.Client {
+	// Default to a 30 second timeout
+	timeout := 30 * time.Second
+	if options.Timeout != 0 {
+		timeout = options.Timeout
+	}
+
 	return &http.Client{
 		Transport: &http.Transport{
 			TLSClientConfig: &tls.Config{
 				InsecureSkipVerify: options.InsecureSkipVerify,
 			},
 		},
-		Timeout: options.Timeout,
+		Timeout: timeout,
 	}
 }
