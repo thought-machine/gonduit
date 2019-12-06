45190ad826d056191d4e55052b9dbd446ea53461
Mike Cripps <agenticarus@gmail.com>
2019-12-06 10:11:56 +0000

    Support project.edit method (#12)

--- entities/projects.go
+++ entities/projects.go
@@ -15,3 +15,80 @@ type Project struct {
 	DateCreated      util.UnixTimestamp `json:"dateCreated"`
 	DateModified     util.UnixTimestamp `json:"dateModified"`
 }
+
+func ProjectNameTransaction(name string) Transaction {
+	return Transaction{
+		Type:  "name",
+		Value: name,
+	}
+}
+
+func ProjectSlugsTransaction(slugs []string) Transaction {
+	return Transaction{
+		Type:  "slugs",
+		Value: slugs,
+	}
+}
+
+func ProjectIconTransaction(icon string) Transaction {
+	return Transaction{
+		Type:  "icon",
+		Value: icon,
+	}
+}
+
+func ProjectColorTransaction(color string) Transaction {
+	return Transaction{
+		Type:  "color",
+		Value: color,
+	}
+}
+
+func ProjectDescriptionTransaction(description string) Transaction {
+	return Transaction{
+		Type:  "description",
+		Value: description,
+	}
+}
+
+func ProjectViewPolicyTransaction(viewPolicy string) Transaction {
+	return Transaction{
+		Type:  "view",
+		Value: viewPolicy,
+	}
+}
+
+func ProjectEditPolicyTransaction(editPolicy string) Transaction {
+	return Transaction{
+		Type:  "edit",
+		Value: editPolicy,
+	}
+}
+
+func ProjectJoinPolicyTransaction(joinPolicy string) Transaction {
+	return Transaction{
+		Type:  "join",
+		Value: joinPolicy,
+	}
+}
+
+func ProjectAddMembersTransaction(members []PHID) Transaction {
+	return Transaction{
+		Type:  "members.add",
+		Value: members,
+	}
+}
+
+func ProjectRemoveMembersTransaction(members []PHID) Transaction {
+	return Transaction{
+		Type:  "members.remove",
+		Value: members,
+	}
+}
+
+func ProjectSetMembersTransaction(members []PHID) Transaction {
+	return Transaction{
+		Type:  "members.set",
+		Value: members,
+	}
+}
