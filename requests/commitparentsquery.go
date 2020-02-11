2849be4549abf1a8d8afbd244c61b0b333d31718
Oliver Muir <ojcmuir@gmail.com>
2020-02-11 16:11:55 +0000

    Remove unused imports (#16)

--- requests/commitparentsquery.go
+++ requests/commitparentsquery.go
@@ -1,7 +1,5 @@
 package requests
 
-import "github.com/thought-machine/gonduit/constants"
-
 // CommitParentsQueryRequest represents a request to the diffusion.commitparentsquery endpoint.
 // One of "repository" or "callsign" is required.  Since "callsign" is deprecated we require "repository".
 type CommitParentsQueryRequest struct {
