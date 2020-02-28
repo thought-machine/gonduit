33ee2e99d6bda153843b21ce242429211ae1a240
Oliver Muir <ojcmuir@gmail.com>
2020-02-28 11:56:34 +0000

    A couple of fixes to get file fetching from diffusion working (#20)
    * Fix file download response type
    
    * Add Request metadata to filecontentsquery request type

--- responses/file_download.go
+++ responses/file_download.go
@@ -1,6 +1,4 @@
 package responses
 
 // FileDownloadResponse represents a response from calling file.download.
-type FileDownloadResponse struct {
-	Result string `json:"result"`
-}
+type FileDownloadResponse string
