950f39a81e43f0748708fecf0d96996970291a92
swestmoreland <swestmoreland@thoughtmachine.net>
2023-11-17 11:10:17 +0000

    Try using a string instead?

--- requests/differential_getrawdiff.go
+++ requests/differential_getrawdiff.go
@@ -3,6 +3,6 @@ package requests
 // DifferentialGetRawDiffRequest represents a request to
 // differential.getrawdiff.
 type DifferentialGetRawDiffRequest struct {
-	DiffID int64 `json:"diffID"`
+	DiffID string `json:"diffID"`
 	Request
 }
