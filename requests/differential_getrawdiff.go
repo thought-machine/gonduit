9421ccb9b17574d49f444b82b46ea7095c8e2333
swestmoreland <swestmoreland@thoughtmachine.net>
2023-11-17 11:24:28 +0000

    Try using an omitempty

--- requests/differential_getrawdiff.go
+++ requests/differential_getrawdiff.go
@@ -3,6 +3,6 @@ package requests
 // DifferentialGetRawDiffRequest represents a request to
 // differential.getrawdiff.
 type DifferentialGetRawDiffRequest struct {
-	DiffID string `json:"diffID"`
+	DiffID string `json:"diffID,omitempty"`
 	Request
 }
