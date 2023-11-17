5758e57d61ce91f564c85268c23d2e7f3b618d41
swestmoreland <swestmoreland@thoughtmachine.net>
2023-11-17 17:42:21 +0000

    Tidy

--- requests/differential_getrawdiff.go
+++ requests/differential_getrawdiff.go
@@ -2,8 +2,7 @@ package requests
 
 import "github.com/thought-machine/gonduit/responses"
 
-// DifferentialGetRawDiffRequest represents a request to
-// differential.getrawdiff.
+// DifferentialGetRawDiffRequest represents a request to differential.getrawdiff.
 type DifferentialGetRawDiffRequest struct {
 	DiffID responses.SearchData `json:"diffID"`
 	Request
