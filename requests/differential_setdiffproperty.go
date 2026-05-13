aa3cc1e04558fb6b25f556549eb5ffd2b6dd625e
Samuel Littley <slittley@thoughtmachine.net>
2026-05-13 13:31:18 +0100

    Add differential.setdiffproperty to gonduit (#41)

---
+++ requests/differential_setdiffproperty.go
@@ -0,0 +1,12 @@
+package requests
+
+// DifferentialSetDiffPropertyRequest represents a request to the differential.setdiffproperty endpoint.
+type DifferentialSetDiffPropertyRequest struct {
+	// ID of the diff (not revision) to set the property on.
+	DiffID uint64 `json:"diff_id"`
+	// Name of the property to set
+	Name string `json:"name"`
+	// Value of the property. Must be a JSON-encoded string representing an object or array of objects.
+	Data string `json:"data"`
+	Request
+}
