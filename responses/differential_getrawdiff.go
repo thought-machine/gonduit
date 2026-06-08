f1d3213270ca3005f652c347eaa5d28b10b8dafb
isobel <isobel@thoughtmachine.net>
2026-06-08 15:45:08 +0100

    Use custom Unmarshal implementation for getrawdiff

--- responses/differential_getrawdiff.go
+++ responses/differential_getrawdiff.go
@@ -1,6 +1,27 @@
 package responses
 
-import "github.com/thought-machine/gonduit/entities"
+import (
+	"encoding/json"
+
+	"github.com/thought-machine/gonduit/entities"
+)
 
 // DifferentialGetRawDiffResponse is the response from calling differential.getrawdiff.
-type DifferentialGetRawDiffResponse *entities.DifferentialRawDiff
+type DifferentialGetRawDiffResponse struct {
+	*entities.DifferentialRawDiff
+}
+
+// UnmarshalJSON handles the fact that Phabricator returns the JSON as a string.
+func (r *DifferentialGetRawDiffResponse) UnmarshalJSON(b []byte) error {
+	var rawString string
+	if err := json.Unmarshal(b, &rawString); err == nil {
+		r.DifferentialRawDiff = &entities.DifferentialRawDiff{
+			Diff: rawString,
+		}
+		return nil
+	}
+
+	// If it is a real JSON object, unmarshal it into the underlying struct normally.
+	r.DifferentialRawDiff = &entities.DifferentialRawDiff{}
+	return json.Unmarshal(b, r.DifferentialRawDiff)
+}
